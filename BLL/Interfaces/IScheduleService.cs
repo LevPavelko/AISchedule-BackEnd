808b9dc03360e8d88c65ca65c5b34d77a0a5e52a
LevPavelko <lev.pavelko.studies@gmail.com>
2026-03-27 12:44:42 +0100

    removed no needed methods

--- BLL/Interfaces/IScheduleService.cs
+++ BLL/Interfaces/IScheduleService.cs
@@ -4,11 +4,6 @@ namespace AIScheduleUI5.BLL.Interfaces;
 
 public interface IScheduleService
 {
-    Task<List<Schedule>> GetAllAsync();
-    Task<Schedule?> GetByIdAsync(Guid id);
-    Task<List<Schedule>> GetByUserIdAsync(Guid userId);
-    Task<Schedule> CreateAsync(Schedule schedule);
-    Task<Schedule?> UpdateAsync(Schedule schedule);
-    Task<bool> DeleteAsync(Guid id);
+
 }
 
